b3b7ec82cd34681ac20ea506c15f76254160dcff
David Barratt <david@davidwbarratt.com>
2020-12-23 19:10:44 +0000

    Remove local credentials

--- backup.sh
+++ backup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-USER="root"
-PASSWORD="S6sJbAqH"
-HOST="db"
-
 databases=`mysql -h $HOST -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
